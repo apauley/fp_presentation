bf6dc5bd74d6b68916879c0d24e3c7d5a8e9c331
Andreas Pauley <apauley+github@gmail.com>
2013-08-17 23:56:31 +0200

    Recursion code samples

---
+++ code/doubleall_recursion.hs
@@ -0,0 +1,11 @@
+module Main where
+
+    doubleAll :: Num a => [a] -> [a]
+    doubleAll []     = []
+    doubleAll (x:xs) = x*2 : doubleAll xs
+
+    main :: IO()
+    main = do
+       let doubledList = doubleAll [6,2,14]
+           strList = [(show i) ++ " " | i <- doubledList]
+       putStrLn $ concat strList
---
+++ code/doubleall_recursion.py
@@ -0,0 +1,7 @@
+def doubleAll(numbers):
+    if not numbers:
+        return []
+    else:
+        first = numbers[0]
+        rest = numbers[1:]
+        return [first * 2] + doubleAll(rest)
